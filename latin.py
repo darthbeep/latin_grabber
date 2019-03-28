50fac3a1b70f594a7acf4f28b2cba60828056ecc
darthbeep <darthbeep@gmail.com>
2019-03-28 01:31:05 -0400

    web parsing works

--- latin.py
+++ latin.py
@@ -1,6 +1,9 @@
 import sys
 import urllib3
+import re
 from bs4 import BeautifulSoup
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def get_html(word):
     http = urllib3.PoolManager()
@@ -11,8 +14,25 @@ def get_html(word):
 def get_definition(soup):
     start = soup.find(id="Latin").parent.findNext("ol")
     lst = start.find_all('li', recursive=False)
+    ret = []
+    found = []
     for element in lst:
-        print(element.get_text().split('\n')[0])
+        elstr = "\t"+element.get_text().split('\n')[0]
+        if "#Latin" in str(element):
+            bonus = element.find_all(href=re.compile("#Latin"))[0]["title"]
+            if bonus not in found:
+                found.append(bonus)
+                toadd = get_definition(get_html(bonus))
+                for b in toadd:
+                    elstr+="\n\t\t\t"+b
 
-soup = get_html("terreo")
-get_definition(soup)
+        ret.append(elstr)
+    return ret
+
+def sort_word(word):
+    soup = get_html(word)
+    print("\t"+word + ":")
+    for word in get_definition(soup):
+        print("\t" + word)
+
+sort_word("amare")
